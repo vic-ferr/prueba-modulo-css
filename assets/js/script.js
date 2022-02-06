68296b5fdcb71fa8078f8b77a959380921d74781
Victor Fernandez <victorhf2014@gmail.com>
2022-02-05 21:36:24 -0300

    advanced css test

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const prueba = 1 + 2 + "hola" 
+console.log(prueba);
+
+const texto1 = "hola";
+const texto2 = "desafio";
+const texto3 = "latam"; 
+
+const saludoGeneral = texto1 + " "+ texto2 + " " +texto3;
+
+
+
+
+
